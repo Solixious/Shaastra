d5fb2c200ca7cfcf8c9e2899fea81686b227be39
Pratyush Choudhary <solixious@gmail.com>
2025-11-14 17:02:36 +0400

    Initial commit

---
+++ src/main/java/dev/indian/Shaastra/ShaastraApplication.java
@@ -0,0 +1,13 @@
+package dev.indian.Shaastra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaastraApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ShaastraApplication.class, args);
+	}
+
+}
